599192b2f50478df103ef2218adea717604c2adb
TheCaptain92 <chrisshirley92@gmail.com>
2022-11-22 00:16:23 -0500

    Add files via upload
    Uploading main.py where all of the code can be found and style.css. The only thing happening in style.css is changing the background color and the color of the text.

---
+++ main.py
@@ -0,0 +1,42 @@
+# Import Libraries
+
+import dash as Dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+import yfinance as yf
+import datetime as dt
+
+from pandas import DataFrame
+
+#Read the dataset
+#data = pd.read_csv('Dataset.csv')
+
+# Initialize Dash App
+app = Dash.Dash()
+
+app.layout = html.Div(children = [html.H1("Financial Dashboard"),
+html.H2("Money in Memes"),html.Div(style={'color': 'black'},children= [
+dcc.Dropdown(id='Ticker-Dropdown',options=["SPY","AAPL","TSLA"],value='SPY')]),
+dcc.Graph(id= 'Ticker-Graph', figure={'layout': go.Layout(paper_bgcolor='rgba(0,0,0,0)',
+plot_bgcolor='rgba(0,0,0,0)')})])
+
+@app.callback(
+    Output(component_id= 'Ticker-Graph', component_property= 'figure'),
+    Input(component_id= 'Ticker-Dropdown', component_property= 'value')
+)
+
+def update_ticker_graph(ticker):
+    data_frame = pd.DataFrame(yf.download(tickers=ticker, period='1y', interval='1d'))
+
+    filtered_df = data_frame.drop(['Open', 'High', 'Low', 'Volume', 'Close'], axis=1)
+
+    figure = px.line(filtered_df)
+    return figure
+
+# Run local server
+if __name__ == "__main__":
+    app.run_server(debug= True)
